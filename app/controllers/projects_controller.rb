9e6c180cc618cdcd124ba0061406d1a45e79f445
Alex Dixon <adixon@adobe.com>
2011-11-12 12:20:08 -0600

    Wired up a current hand route and hacked it in to render the story show view for the selected story.  Still need to fix things up so that voting doesn't reroute to the story.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -16,4 +16,17 @@ class ProjectsController < ApplicationController
     hand.save!
     head 200 
   end
+
+  def current_hand
+    @project = PivotalPlanningPoker::Project.find(params[:id], @user.token)
+    @current_hand = CurrentHand.find_by_project_id(params[:id])
+    @story = PivotalPlanningPoker::Story.find(params[:id], @current_hand.story_id, @user.token) unless @current_hand.nil?
+    Game.create_or_update(@story.story_id, @story.estimate) unless @story.nil?
+    if @story.nil?
+      flash[:error] = 'No hand has been set'
+      redirect_to :action => :show, :id => params[:id]
+    else
+      render '/stories/show'
+    end
+  end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,11 @@
 class Game < ActiveRecord::Base
   serialize :estimates
+
+  def self.create_or_update(tracker_story_id, tracker_estimate)
+    if game = Game.first(:conditions => { :tracker_story_id => tracker_story_id })
+      game.update_attribute(:tracker_estimate, tracker_estimate)
+    else
+      Game.create(:tracker_story_id => tracker_story_id, :tracker_estimate => tracker_estimate, :estimates => {})
+    end
+  end
 end
